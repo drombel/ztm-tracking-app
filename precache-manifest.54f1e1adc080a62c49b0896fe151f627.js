44184019d9fd826c5d524d9117cd23a1da70e07d
drombel <dawid.rombel@gmail.com>
2020-09-02 01:50:14 +0200

    Updates

---
+++ precache-manifest.54f1e1adc080a62c49b0896fe151f627.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23a26ce92a783dff9439fa0c18b5042a",
+    "url": "/ztm-tracking-app/index.html"
+  },
+  {
+    "revision": "f07b4a5fed20857a8863",
+    "url": "/ztm-tracking-app/static/css/main.0c40397a.chunk.css"
+  },
+  {
+    "revision": "8e78f7b14bf31511c9ce",
+    "url": "/ztm-tracking-app/static/js/2.d64011f3.chunk.js"
+  },
+  {
+    "revision": "031b62b957b4fb917cd0ec81e2c8bd7b",
+    "url": "/ztm-tracking-app/static/js/2.d64011f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f07b4a5fed20857a8863",
+    "url": "/ztm-tracking-app/static/js/main.b3d16077.chunk.js"
+  },
+  {
+    "revision": "191164356623012e7e9a",
+    "url": "/ztm-tracking-app/static/js/runtime-main.2bf01adc.js"
+  }
+]);
